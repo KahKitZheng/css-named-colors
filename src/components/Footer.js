17a9afb15943723302eccb8f55ffe67fc7754ff3
Kah Kit Zheng <kahkitzheng@gmail.com>
2021-09-29 16:07:15 +0200

    Update links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,12 +16,26 @@ const StyledFooter = styled.footer`
   }
 `;
 
+const Bullet = styled.span`
+  margin: 0 0.5rem;
+  opacity: 0.3;
+  color: white;
+`;
+
 const Footer = () => (
   <StyledFooter>
     <p>
       Made by{" "}
-      <a href="https://github.com/KahKitZheng" target="_blank">
+      <a href="https://github.com/KahKitZheng" target="_blank" rel="noreferrer">
         Kah Kit Zheng
+      </a>
+      <Bullet>&#8226;</Bullet>
+      <a
+        href="https://github.com/KahKitZheng/css-named-colors"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github Repo
       </a>
     </p>
   </StyledFooter>
